6142bb1f714368a8156e6da8c5a37f13c8ed1a6f
Juan Melo <jcmelop@ufpso.edu.co>
2023-06-23 10:49:47 -0500

    subiendo proyecto de agregar y quitar tareas usando la comunicacion entre padre e hijo

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import TaskComponent from './components/TaskComponent.vue';
+import { ref } from 'vue';
+const taskList = ref([
+    'Levantarse',
+    'Cepillarse',
+    'Alimentarse',
+    
+  ])
+  
+let newTask
+
+function addTask() {
+taskList.value.unshift(newTask);
+newTask=undefined
+}
+const deleteTask =(index)=>{
+taskList.value.splice(index,1)
+}
+</script>
+
+<template>
+
+<div class="px-4 py-3 mx-auto mt-12 space-y-2 overflow-hidden
+rounded shadow-md bg-gray-100 w-96">
+  <h1 class="text-xl font-bold">
+    Lista de tareas
+  </h1>
+  <div class="flex" >
+    <input type="text" class="w-4/5 h-8 px-2 py-1 border border-r-0
+    border-green-600 text-lg font-semibold text-center rounded
+    rounded-r-none" v-model="newTask">
+    <span class="w-1/5 h-8 py-1 text-white border-green-500 bg-green-700 text-center rounded rounded-l-none
+     leading-none hover:bg-green-500 hover:cursor-pointer" @click="addTask">
+      +
+    </span>
+  </div>
+  <TaskComponent v-for="(task,index) in taskList" 
+  :name="task" :index="index"
+  @deleteTask="deleteTask"/>
+</div>
+</template>
---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: '-'
+     },
+    index: {
+    type:Number
+    }
+})
+const emit = defineEmits([
+    'deleteTask'
+])
+const deleteTask =(index)=>{
+    emit('deleteTask',index)
+}
+</script>
+<template>
+    <div class="flex justify-between px-4 py-2 
+    bg-gray-200 rounded">
+    {{ props.name }}
+        <span @click="deleteTask(props.index)" class="inline-block px-2 text-white
+        bg-red-800 rounded hover:bg-red-400 hover:cursor-pointer">
+            x
+        </span>
+    </div>
+</template>
